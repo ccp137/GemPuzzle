00a0d05d6acccb3e899da68e3e7c927ddd152b6d
Xiaotian Zhu <zhux2008@gmail.com>
2014-11-18 14:37:47 -0500

    added team folder for holding what we all agree upon.

---
+++ team/main.cpp
@@ -0,0 +1,25 @@
+//
+// main.cpp -- entry point for testing GemPuzzle project
+//
+// Written by Xiaotian Zhu
+//
+
+#include <iostream>
+#include "Puzzle.h"
+#include "Game.h"
+#include "tests.h"
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    /* initialize random seed:
+    srand(time(NULL));*/
+
+    //testPuzzle();
+
+    testGame();
+
+    return 0;
+}
