4b36bc8d704ec4b09988c6dd14e3d5f4e50e2be1
CHENGPING CHAI <cxc754@psu.edu>
2014-11-19 13:59:25 -0500

    copy code from team folder

---
+++ cchai/main.cpp
@@ -0,0 +1,23 @@
+//
+// main.cpp -- entry point for testing GemPuzzle project
+//
+// Written by Xiaotian Zhu
+//
+
+#include <iostream>
+#include "Puzzle.h"
+#include "Game.h"
+#include "tests.h"
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+
+    testPuzzle();
+
+    testGame();
+
+    return 0;
+}
---
+++ cchai/tests.h
@@ -0,0 +1,11 @@
+//
+// tests.h -- declaration of functions doing tests
+//
+// Written by Xiaotian
+//
+
+#pragma once
+
+void testPuzzle();
+
+void testGame();
